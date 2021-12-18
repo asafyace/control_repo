822b5f5c427d8ba97f5a92513d9b4bd7885e6e19
Asaf Yaron <44402345+asafyace@users.noreply.github.com>
2021-12-18 20:01:42 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft {
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
-    before => Service[‘minecraft’],
+    before => Service['minecraft'],
   }
 
 
